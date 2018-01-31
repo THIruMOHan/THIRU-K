db81b1a1766b7a6b3d08f892186a27292f1e817e
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-01-31 16:19:39 +0530

    THIRU

---
+++ THIRU.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("positive");
+}
+else
+{
+printf("negative");
+}
+}
+getch();
+return o;
+}
