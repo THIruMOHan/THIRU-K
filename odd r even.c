bd55be66915e6b4a7a9b32b19bba237024a4bc55
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-01-31 16:45:52 +0530

    odd r even

---
+++ odd r even.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("EVEN");
+}
+else
+{
+printf("ODD");
+}
+getch();
+return 0:
+}
